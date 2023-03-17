726f0288204d7c550b783024e6c9e6c10abb1505
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-03-16 23:34:12 -0300

    Classe Cliente para brincar com OO

---
+++ formacao-php-tw/02-php-oo-parte-01/src/Classes/Cliente.php
@@ -0,0 +1,16 @@
+<?php
+
+class Cliente 
+{
+    // Atributos
+    public $nome;
+    public $idade;
+    public $endereco;
+    public $telefone;
+    
+    public function comprar()
+    {
+        echo "Cliente realizou uma compra";
+    }
+
+}
