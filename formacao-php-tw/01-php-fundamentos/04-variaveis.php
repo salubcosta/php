f82646b23077f716b515f5cf41f1413b35b84880
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-03-17 20:03:28 -0300

    Constantes no PHP

---
+++ formacao-php-tw/01-php-fundamentos/04-variaveis.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Sempre começa com $
+ * Pode começar com $_
+ * Pode com letras $a $b
+ * Nunca poderá inicar com números. Exemplo: $1pessoa;
+ */
+
+ $email = "mail@mail.com";
+
+ echo $email;
+
+ $email = "e-mail@mail.com";
+
+ echo "<br>$email";
---
+++ formacao-php-tw/01-php-fundamentos/05-constantes.php
@@ -0,0 +1,20 @@
+<?php
+
+// Como declarar e atribuir constantes
+define("ESCOLA", "Treinaweb"); // Escopo global
+
+echo ESCOLA;
+
+// Outra forma de declarar e atribuir
+const LINGUAGEM = 'PHP'; // Escopo de bloco
+
+echo "<br>".LINGUAGEM;
+
+
+/** 
+ * Algumas observações quanto ao uso:
+ * Assim como as variáveis, fazem distinção entre letras maiúsculas e minúsculas;
+ * O nome de uma constante deve iniciar com letra ou sublinhado;
+ * Constantes não são prefixadas com o caractere $;
+*/
+
