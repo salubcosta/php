6dbad9a7be005174d96df0ce4fa9f5cfa0be4b66
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-03-18 23:55:36 -0300

    Operadores aritméticos

---
+++ formacao-php-tw/01-php-fundamentos/08-operadores.php
@@ -0,0 +1,26 @@
+<?php
+
+$soma = 4 + 6;
+$subtracao = 10 - 3;
+$multiplicacao = 3 * 4;
+$divisao = 81 / 9;
+$resto = 82 % 9;
+$exponenciacao = 10 ** 2;
+
+var_dump($soma);
+echo "<br>";
+
+var_dump($subtracao);
+echo "<br>";
+
+var_dump($multiplicacao);
+echo "<br>";
+
+var_dump($divisao);
+echo "<br>";
+
+var_dump($resto);
+echo "<br>";
+
+var_dump($exponenciacao);
+echo "<br>";
