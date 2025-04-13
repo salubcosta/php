4a8fd0518694ee04686664dc5ce45ceae923d91d
Salumão Costa <salubcosta@gmail.com>
2025-04-13 16:38:30 -0300

    iniciado novo projeto de bookwise

---
+++ php-rocketseat/02-estrutura-base-do-projeto/bookwise/dados.php
@@ -0,0 +1,13 @@
+<?php
+
+$livros = [
+    ['id' => 1, 'titulo' => 'Senhor dos Anéis', 'autor' => 'J.R.R. Tolkein', 'descricao' => 'A história é ambientada na terra média lorem ipsum'],
+    ['id' => 2, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'Uma distopia futurista que explora temas de lorem ipsum'],
+    ['id' => 3, 'titulo' => 'O Grande Gatsby', 'autor' => 'F. Scott Fitzgerald', 'descricao' => 'Um retrato da Era do Jazz e uma lorem ipsum'],
+    ['id' => 4, 'titulo' => 'Orgulho e Preconceito', 'autor' => 'Jane Austen', 'descricao' => 'Um romance clássico que explora as lorem ipsum'],
+    ['id' => 5, 'titulo' => 'Moby Dick', 'autor' => 'Herman Melville', 'descricao' => 'Uma épica aventura marítima que seja a lorem ipsum'],
+    ['id' => 6, 'titulo' => 'Guerra e Paz', 'autor' => 'Liev Tolstói', 'descricao' => 'Um extenso romance histórico que narra a vida lorem ipsum'],
+    ['id' => 7, 'titulo' => 'O Apanhador no Campo de Centeio', 'autor' => 'J.D. Salinger', 'descricao' => 'Um relato sobre a angústia lorem ipsum'],
+    ['id' => 8, 'titulo' => 'Crime e Castigo', 'autor' => 'Fiódor Dostoiévski', 'descricao' => 'Um estudo psicológico profundo sobre lorem ipsum'],
+    ['id' => 9, 'titulo' => 'Ulisses', 'autor' => 'James Joyce', 'descricao' => 'Uma obra-prima modernista que seja um dia na vida de lorem ipsum']
+];
