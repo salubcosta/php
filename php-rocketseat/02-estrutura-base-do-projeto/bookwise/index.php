67898c21201c06e3a09223ffead496fbc2831ccc
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-04-15 22:18:16 -0300

    reestruturando o projeto e adicionando novos detalhes

--- php-rocketseat/02-estrutura-base-do-projeto/bookwise/index.php
+++ php-rocketseat/02-estrutura-base-do-projeto/bookwise/index.php
@@ -1,58 +1,7 @@
-<?php require("dados.php"); ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Wise</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
+require "dados.php";
 
-<body class="bg-stone-950 text-stone-200">
-    <header class="bg-stone-900">
-        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
-            <div class="font-bold text-xl tracking-wide">Book Wise</div>
-            <ul class="flex space-x-4 font-bold">
-                <li><a href="/" class="text-lime-500">Explorar</a></li>
-                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
-            </ul>
+$view = "index";
 
-            <ul>
-                <li><a href="/login.php" class="hover:underline">Fazer login</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <main class="mx-auto max-w-screen-lg space-y-6">
-        <!-- <h1 class="mt-6 font-bold text-lg">Explorar</h1> -->
-        <form class="w-full flex space-x-2 mt-6">
-            <input
-                type="text"
-                class=" border-stone-800 border-2 rounded-md bg-stone-900 
-                        text-sm focus:outline-none px-2 py-1"
-                placeholder="Pesquisar..."
-                name="pesquisar">
-            <button type="submit">🔎</button>
-        </form>
-        <!-- Lista de Livros -->
-        <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <!--  Livro -->
-            <?php foreach($livros as $livro): ?>
-                <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                    <div class="flex">
-                        <div class="w-1/3">imagem</div>
-                        <div class="space-y-1">
-                            <a href="/livro.php?id=<?=$livro['id']?>" class="font-semibold hover:underline"><?=$livro['titulo'];?></a>
-                            <div class="text-xs italic"><?=$livro['autor'];?></div>
-                            <div class="text-xs italic">⭐⭐⭐⭐⭐(3 avaliacao)</div>
-                        </div>
-                    </div>
-                    <div class="text-sm mt-2"><?=$livro['descricao']?></div>
-                </div>
-            <?php endforeach; ?>
-        </section>
-    </main>
-</body>
-
-</html>
+require "views/template/app.php";
---
+++ php-rocketseat/02-estrutura-base-do-projeto/bookwise/views/template/app.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Wise</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-stone-950 text-stone-200">
+    <header class="bg-stone-900">
+        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
+            <div class="font-bold text-xl tracking-wide">Book Wise</div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-lime-500">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="/login.php" class="hover:underline">Fazer login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="mx-auto max-w-screen-lg space-y-6">
+        <?php require "views/{$view}.view.php"; ?>
+    </main>
+</body>
+
+</html>
